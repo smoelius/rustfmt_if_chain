d92ae06a72adb3b0757f6a49e75b645f6aef31dd
Samuel E. Moelius III <sam@moeli.us>
2022-01-24 19:21:16 -0500

    Improve argument handling

--- tests/example.rs
+++ tests/example.rs
@@ -1,23 +1,25 @@
 use assert_cmd::Command;
 use std::{fs::copy, path::Path};
-use tempfile::NamedTempFile;
+use tempfile::tempdir;
 
 #[test]
 fn example() {
     let examples = Path::new(env!("CARGO_MANIFEST_DIR")).join("examples");
 
-    let tempfile = NamedTempFile::new().unwrap();
+    let tempdir = tempdir().unwrap();
 
-    copy(examples.join("before.rs"), tempfile.path()).unwrap();
+    let tempfile = tempdir.path().join("before.rs");
+
+    copy(examples.join("before.rs"), &tempfile).unwrap();
 
     Command::cargo_bin("rustfmt_if_chain")
         .unwrap()
-        .arg(tempfile.path())
+        .arg(&tempfile)
         .assert()
         .success();
 
     Command::new("diff")
-        .args(&[tempfile.path(), examples.join("after.rs").as_path()])
+        .args(&[tempfile, examples.join("after.rs")])
         .assert()
         .success();
 }
