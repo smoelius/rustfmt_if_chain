93a8f1f33c219f516772938ea1371d78e5524827
Samuel E. Moelius III <sam@moeli.us>
2022-01-25 21:48:57 -0500

    Add bechmark

--- tests/example.rs
+++ tests/example.rs
@@ -1,9 +1,14 @@
+#![cfg_attr(nightly, feature(bench_black_box, test))]
+
+#[cfg(nightly)]
+extern crate test;
+
 use assert_cmd::Command;
 use std::{fs::copy, path::Path};
 use tempfile::tempdir;
 
 #[test]
-fn example() {
+fn example_test() {
     let examples = Path::new(env!("CARGO_MANIFEST_DIR")).join("examples");
 
     let tempdir = tempdir().unwrap();
@@ -22,4 +27,24 @@ fn example() {
         .args(&[tempfile, examples.join("after.rs")])
         .assert()
         .success();
+}
+
+#[cfg(nightly)]
+#[bench]
+fn example_bench(bencher: &mut test::Bencher) {
+    let examples = Path::new(env!("CARGO_MANIFEST_DIR")).join("examples");
+
+    let tempdir = tempdir().unwrap();
+
+    let tempfile = tempdir.path().join("before.rs");
+
+    bencher.iter(|| {
+        copy(examples.join("before.rs"), &tempfile).unwrap();
+
+        Command::cargo_bin("rustfmt_if_chain")
+            .unwrap()
+            .arg(&tempfile)
+            .assert()
+            .success();
+    });
 }
