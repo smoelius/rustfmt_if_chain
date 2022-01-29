22eefe251569899783fc1e18a8291c0875683b3f
Samuel E. Moelius III <sam@moeli.us>
2022-01-28 21:28:41 -0500

    Improve Clippy test

--- tests/clippy.rs
+++ tests/clippy.rs
@@ -1,5 +1,5 @@
 use assert_cmd::Command;
-use std::ffi::OsStr;
+use std::{ffi::OsStr, fs::OpenOptions, io::Write};
 use tempfile::tempdir_in;
 use walkdir::WalkDir;
 
@@ -8,6 +8,8 @@ const CLIPPY_URL: &str = "https://github.com/rust-lang/rust-clippy";
 #[test]
 fn clippy() {
     let tempdir = tempdir_in(".").unwrap();
+
+    let crashes = tempdir.path().join("tests").join("ui").join("crashes");
 
     Command::new("git")
         .args(&["clone", CLIPPY_URL, &tempdir.path().to_string_lossy()])
@@ -20,12 +22,29 @@ fn clippy() {
         if path.extension() != Some(OsStr::new("rs")) {
             continue;
         }
-        Command::cargo_bin("rustfmt_if_chain")
+        let assert = Command::cargo_bin("rustfmt_if_chain")
             .unwrap()
-            .args(&["--preformat-failure-is-warning", &path.to_string_lossy()])
-            .assert()
-            .success();
+            .arg(path)
+            .assert();
+        assert!(
+            assert.try_success().is_ok()
+                || path.starts_with(&crashes)
+                || path
+                    .file_name()
+                    .unwrap()
+                    .to_string_lossy()
+                    .starts_with("lib."),
+            "path = {:?}",
+            path
+        );
     }
+
+    let mut file = OpenOptions::new()
+        .append(true)
+        .write(true)
+        .open(tempdir.path().join("Cargo.toml"))
+        .unwrap();
+    writeln!(file, "[workspace]").unwrap();
 
     Command::new("cargo")
         .args(&["build", "--tests"])
