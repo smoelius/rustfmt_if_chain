10092fbbe2ceb69d40f088e7a4e2acf64ac30963
Samuel Moelius <sam@moeli.us>
2023-03-21 09:10:04 -0400

    Fix `clippy` test

--- tests/clippy.rs
+++ tests/clippy.rs
@@ -5,6 +5,28 @@ use walkdir::WalkDir;
 
 const CLIPPY_URL: &str = "https://github.com/rust-lang/rust-clippy";
 
+// smoelius: `needless_return.rs` uses the `do` keyword (see:
+// https://github.com/rust-lang/rust-clippy/pull/10109), which does not seem to be supported by
+// `syn`.
+// smoelius: This problem persists with `syn` 2.0.
+
+// smoelius: Something weird is going on with `clippy_lints/src/derive.rs`. Remove the exception
+// once the following is resolved: https://github.com/rust-lang/rustfmt/issues/5700
+
+// smoelius: From: https://github.com/dtolnay/syn/releases/tag/2.0.0
+//
+//   Support for `box expr` syntax has been deleted, as it has been deleted recently from rustc.
+//
+// This is the reason for the boxed_local.rs, no_effect.rs, and unnecessary_operation.rs exceptions.
+const EXCEPTIONS: &[&str] = &[
+    "clippy_lints/src/derive.rs",
+    "clippy_lints/src/lib.deprecated.rs",
+    "tests/ui/boxed_local.rs",
+    "tests/ui/needless_return.rs",
+    "tests/ui/no_effect.rs",
+    "tests/ui/unnecessary_operation.rs",
+];
+
 #[test]
 fn clippy() {
     let tempdir = tempdir_in(".").unwrap();
@@ -19,17 +41,9 @@ fn clippy() {
     for entry in WalkDir::new(&tempdir) {
         let entry = entry.unwrap();
         let path = entry.path();
-        // smoelius: `needless_return.rs` uses the `do` keyword (see:
-        // https://github.com/rust-lang/rust-clippy/pull/10109), which does not seem to be supported
-        // by `syn`.
-        // smoelius: Something weird is going on with `clippy_lints/src/derive.rs`. Remove the
-        // exception once the following is resolved:
-        // https://github.com/rust-lang/rustfmt/issues/5700
         if path.extension() != Some(OsStr::new("rs"))
             || path.starts_with(&crashes)
-            || path.file_name() == Some(OsStr::new("derive.rs"))
-            || path.file_name() == Some(OsStr::new("lib.deprecated.rs"))
-            || path.file_name() == Some(OsStr::new("needless_return.rs"))
+            || EXCEPTIONS.iter().any(|child| path.ends_with(child))
         {
             continue;
         }
