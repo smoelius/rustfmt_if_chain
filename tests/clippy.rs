df9ad0506f08c34101d0b20e2106cd36516e6980
Samuel Moelius <sam@moeli.us>
2023-09-25 13:16:47 -0400

    Various improvements to Clippy test

--- tests/clippy.rs
+++ tests/clippy.rs
@@ -1,29 +1,40 @@
 use assert_cmd::Command;
-use std::{ffi::OsStr, fs::OpenOptions, io::Write};
+use std::{env::remove_var, ffi::OsStr, fs::OpenOptions, io::Write};
 use tempfile::tempdir_in;
 use walkdir::WalkDir;
 
+#[ctor::ctor]
+fn initialize() {
+    remove_var("CARGO_TERM_COLOR");
+    remove_var("RUSTUP_TOOLCHAIN");
+}
+
 const CLIPPY_URL: &str = "https://github.com/rust-lang/rust-clippy";
 
-// smoelius: `needless_return.rs` uses the `do` keyword (see:
-// https://github.com/rust-lang/rust-clippy/pull/10109), which does not seem to be supported by
-// `syn`.
-// smoelius: This problem persists with `syn` 2.0.
-
-// smoelius: Something weird is going on with `clippy_lints/src/derive.rs`. Remove the exception
-// once the following is resolved: https://github.com/rust-lang/rustfmt/issues/5700
-
-// smoelius: From: https://github.com/dtolnay/syn/releases/tag/2.0.0
-//
-//   Support for `box expr` syntax has been deleted, as it has been deleted recently from rustc.
-//
-// This is the reason for the boxed_local.rs, no_effect.rs, and unnecessary_operation.rs exceptions.
 const EXCEPTIONS: &[&str] = &[
+    // smoelius: Something weird is going on with `clippy_lints/src/derive.rs`. Remove the exception
+    // once the following is resolved: https://github.com/rust-lang/rustfmt/issues/5700
     "clippy_lints/src/derive.rs",
+    // ???
     "clippy_lints/src/lib.deprecated.rs",
+    // smoelius: The line causing problems in clippy_utils/src/consts.rs contains a `matches!`
+    // invocation. So I suspect the underlying cause is the same as that affecting
+    // clippy_lints/src/derive.rs.
+    "clippy_utils/src/consts.rs",
+    // ???
     "tests/ui/boxed_local.rs",
+    // smoelius: `needless_return.rs` uses the `do` keyword (see:
+    // https://github.com/rust-lang/rust-clippy/pull/10109), which does not seem to be supported by
+    // `syn`.
+    // smoelius: This problem persists with `syn` 2.0.
     "tests/ui/needless_return.rs",
+    // smoelius: From: https://github.com/dtolnay/syn/releases/tag/2.0.0
+    //
+    //   Support for `box expr` syntax has been deleted, as it has been deleted recently from rustc.
+    //
+    // This is the reason for the boxed_local.rs, no_effect.rs, and unnecessary_operation.rs exceptions.
     "tests/ui/no_effect.rs",
+    // ???
     "tests/ui/unnecessary_operation.rs",
 ];
 
@@ -50,7 +61,6 @@ fn clippy() {
         Command::cargo_bin("rustfmt_if_chain")
             .unwrap()
             .current_dir(&tempdir)
-            .env_remove("RUSTUP_TOOLCHAIN")
             .arg(path)
             .assert()
             .success();
@@ -64,9 +74,8 @@ fn clippy() {
     writeln!(file, "[workspace]").unwrap();
 
     Command::new("cargo")
-        .args(["build", "--all-features", "--all-targets"])
+        .args(["check", "--all-features", "--all-targets"])
         .current_dir(&tempdir)
-        .env_remove("RUSTUP_TOOLCHAIN")
         .assert()
         .success();
 }
